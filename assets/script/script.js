127137cb8b01caed78132968fbde3a24c98d5d91
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-09 02:58:04 +0800

    Update file

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,7 +6,3 @@ function Book(title, author, pages, status) {
   this.pages = pages;
   this.status = status;
 }
-
-let book1 = new Book ("Harry Potter", "J.K. Rowling", "295", "Read");
-
-console.log(book1.name);
