4265389759482656f861e7ce378a6872bee87e70
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-11 21:09:02 +0800

    Create a function for the the favorites and count it

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,5 @@
 const myLibrary = [];
-const formInput = document.querySelector('.pop-bg');
-const form = document.getElementById('form')
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
+let favoriteCount = 0;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -26,9 +22,28 @@ function displayBooks(){
   `;
 
   myLibrary.forEach((book, index) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
+    if(book.isFavorite){
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <div class="text-container">
+        <div class="book-title">
+          ${book.title}
+        </div>
+        <div class="book-author"><span>Book by</span><br> ${book.author}</div>
+        <div class="book-pages">${book.pages} Pages</div>
+        <div class="book-status">${book.read}</div>
+        <div class="features">
+          <i class="fa-regular fa-heart fa-hearts" style="font-weight: 700;" onclick="addToFavorites(${index})"></i>
+          <i class="fa-regular fa-circle" onclick="changeStatus(${index})"></i>
+          <i class="fa-solid fa-trash" onclick="removeBook(${index})"></i>
+        </div>
+      `
+      cardContainer.append(card);
+    } else{
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
       <div class="text-container">
       <div class="book-title">
         ${book.title}
@@ -37,22 +52,50 @@ function displayBooks(){
       <div class="book-pages">${book.pages} Pages</div>
       <div class="book-status">${book.read}</div>
       <div class="features">
-        <i class="fa-regular fa-heart fa-hearts"></i>
-        <i class="fa-regular fa-circle"></i>
+        <i class="fa-regular fa-heart fa-hearts" onclick="addToFavorites(${index})"></i>
+        <i class="fa-regular fa-circle" onclick="changeStatus(${index})"></i>
         <i class="fa-solid fa-trash" onclick="removeBook(${index})"></i>
       </div>
-    `
-    cardContainer.append(card);
+      `
+      cardContainer.append(card);
+    }
   });
 }
 
-function showForm(){
-  formInput.style.display = "block";
+function addToFavorites(index){
+  if(myLibrary[index].isFavorite === undefined){
+    myLibrary[index].isFavorite = true;
+    favoriteCount++;
+  } else if(myLibrary[index].isFavorite === true){
+    myLibrary[index].isFavorite = false;
+    favoriteCount--;
+  } else if(myLibrary[index].isFavorite === false){
+    myLibrary[index].isFavorite = true;
+    favoriteCount++;
+  }
+  console.log(favoriteCount);
+  displayBooks();
+}
+
+function changeStatus(index){
+  if(myLibrary[index].read === "Read"){
+    myLibrary[index].read = "Not Started";
+  } else if(myLibrary[index].read === "Not Started"){
+    myLibrary[index].read = "In Progress";
+  } else{
+    myLibrary[index].read = "Read";
+  }
+
+  displayBooks();
 }
 
 function removeBook(index){
   myLibrary.splice(index, 1);
   displayBooks();
+}
+
+function showForm(){
+  document.querySelector('.pop-bg').style.display = "block";
 }
 
 function checkLibrary(){
@@ -61,15 +104,13 @@ function checkLibrary(){
 
 document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
-  const titleVal = title.value;
-  const authorVal = author.value;
-  const pagesVal = pages.value;
-  const isRead = document.querySelector('input[name="is-read"]:checked');
-  const isReadVal = isRead.value;
+  const titleVal = document.getElementById('title').value;
+  const authorVal = document.getElementById('author').value;
+  const pagesVal = document.getElementById('pages').value;
+  const isReadVal = document.querySelector('input[name="is-read"]:checked').value;
 
   let book = new Book(titleVal, authorVal, pagesVal, isReadVal);
   addBookToLibrary(book);
-  formInput.style.display = "none";
-  form.reset();
-})
-
+  document.querySelector('.pop-bg').style.display = "none";
+  document.getElementById('form').reset();
+});
