110251a176bd6d83312519b03cac34f95d56d325
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-11 23:51:25 +0800

    Create a logic to change the color of the status and log the items used

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,4 @@
 const myLibrary = [];
-let favoriteCount = 0;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,14 +10,15 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
   displayBooks();
+  updateDisplayCount()
 }
 
 function displayBooks(){
   const cardContainer = document.getElementById('card-container');
   cardContainer.innerHTML = `
-  <div class="card create-card" onclick="showForm()">
-    <i class="fa-solid fa-plus"></i>
-  </div>
+    <div class="card create-card" onclick="showForm()">
+      <i class="fa-solid fa-plus"></i>
+    </div>
   `;
 
   myLibrary.forEach((book, index) => {
@@ -32,32 +32,34 @@ function displayBooks(){
         </div>
         <div class="book-author"><span>Book by</span><br> ${book.author}</div>
         <div class="book-pages">${book.pages} Pages</div>
-        <div class="book-status">${book.read}</div>
+        <div class="book-status" id="reading-status-${index}">${book.read}</div>
         <div class="features">
           <i class="fa-regular fa-heart fa-hearts" style="font-weight: 700;" onclick="addToFavorites(${index})"></i>
-          <i class="fa-regular fa-circle" onclick="changeStatus(${index})"></i>
+          <i class="fa-regular fa-circle" id="status-${index}" onclick="changeStatus(${index})"></i>
           <i class="fa-solid fa-trash" onclick="removeBook(${index})"></i>
         </div>
-      `
+      `;
       cardContainer.append(card);
+      updateStatusDisplay(index);
     } else{
       const card = document.createElement('div');
       card.classList.add('card');
       card.innerHTML = `
-      <div class="text-container">
-      <div class="book-title">
-        ${book.title}
-      </div>
-      <div class="book-author"><span>Book by</span><br> ${book.author}</div>
-      <div class="book-pages">${book.pages} Pages</div>
-      <div class="book-status">${book.read}</div>
-      <div class="features">
-        <i class="fa-regular fa-heart fa-hearts" onclick="addToFavorites(${index})"></i>
-        <i class="fa-regular fa-circle" onclick="changeStatus(${index})"></i>
-        <i class="fa-solid fa-trash" onclick="removeBook(${index})"></i>
-      </div>
-      `
+        <div class="text-container">
+        <div class="book-title">
+          ${book.title}
+        </div>
+        <div class="book-author"><span>Book by</span><br> ${book.author}</div>
+        <div class="book-pages">${book.pages} Pages</div>
+        <div class="book-status" id="reading-status-${index}">${book.read}</div>
+        <div class="features">
+          <i class="fa-regular fa-heart fa-hearts" onclick="addToFavorites(${index})"></i>
+          <i class="fa-regular fa-circle" id="status-${index}" onclick="changeStatus(${index})"></i>
+          <i class="fa-solid fa-trash" onclick="removeBook(${index})"></i>
+        </div>
+      `;
       cardContainer.append(card);
+      updateStatusDisplay(index);
     }
   });
 }
@@ -65,16 +67,14 @@ function displayBooks(){
 function addToFavorites(index){
   if(myLibrary[index].isFavorite === undefined){
     myLibrary[index].isFavorite = true;
-    favoriteCount++;
   } else if(myLibrary[index].isFavorite === true){
     myLibrary[index].isFavorite = false;
-    favoriteCount--;
   } else if(myLibrary[index].isFavorite === false){
     myLibrary[index].isFavorite = true;
-    favoriteCount++;
   }
-  console.log(favoriteCount);
+
   displayBooks();
+  updateDisplayCount()
 }
 
 function changeStatus(index){
@@ -87,19 +87,70 @@ function changeStatus(index){
   }
 
   displayBooks();
+  updateStatusDisplay(index);
+}
+
+function updateStatusDisplay(index){
+  let circleStatusClr = document.getElementById(`status-${index}`);
+  let readingStatusTxtClr = document.getElementById(`reading-status-${index}`);
+
+  if(myLibrary[index].read === "Read"){
+    readingStatusTxtClr.style.color = '#428745';
+    circleStatusClr.style.color = '#428745';
+    circleStatusClr.style.fontWeight = '700';
+  } else if(myLibrary[index].read === "Not Started"){
+    readingStatusTxtClr.style.color = '#ff0000';
+    circleStatusClr.style.color = '#ff0000';
+    circleStatusClr.style.fontWeight = '700';
+  } else{
+    readingStatusTxtClr.style.color = '#c18e31';
+    circleStatusClr.style.color = '#c18e31';
+    circleStatusClr.style.fontWeight = '700';
+  }
+
+  updateDisplayCount()
+}
+
+function updateDisplayCount(){
+  let isFavoriteFiltered = myLibrary.filter(item => {
+    return item.isFavorite == true;
+  }).length;
+
+  let haveRead = myLibrary.filter(item => {
+    return item.read == "Read";
+  }).length;
+
+  let isReading = myLibrary.filter(item => {
+    return item.read == "In Progress";
+  }).length;
+
+  let willRead = myLibrary.filter(item => {
+    return item.read == "Not Started";
+  }).length;
+
+  const parentDisplay = document.getElementById('count-display').children;
+  parentDisplay.item(0).innerHTML = `
+    <i class="fa-solid fa-heart" id="heart"></i> Favorited - ${isFavoriteFiltered}
+  `;
+  parentDisplay.item(1).innerHTML = `
+    <i class="fa-solid fa-flag"></i> Books Read - ${haveRead}
+  `;
+  parentDisplay.item(2).innerHTML = `
+    <i class="fa-solid fa-hourglass-start"></i> In Progress - ${isReading}
+  `;
+  parentDisplay.item(3).innerHTML = `
+    <i class="fa-solid fa-flag-checkered"></i> Not Started - ${willRead}
+  `;
 }
 
 function removeBook(index){
   myLibrary.splice(index, 1);
   displayBooks();
+  updateDisplayCount()
 }
 
 function showForm(){
   document.querySelector('.pop-bg').style.display = "block";
-}
-
-function checkLibrary(){
-  console.log(myLibrary);
 }
 
 document.getElementById('form').addEventListener('submit', (event) => {
